35946c8b79096b83b661e1b68d7a6bec34239c0d
jakoblumpkin <lumpkinjakobr@gmail.com>
2021-03-23 17:05:11 -0500

    lab6

--- src/App.js
+++ src/App.js
@@ -8,18 +8,28 @@ class App extends React.Component {
     super(props);
     this.state={
       location: {},
-      city: ''
+      city: '',
+      display: false,
+      srclink: ''
     }
   }
 
 
   locationSearch = async(e) => {
     e.preventDefault();
-    this.setState({ city: e.target.lat.value });
-    const url=`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=&format=json`;
+    const url=`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.city}&format=json`;
     console.log(url);
     const location = await axios.get(url);
-    console.log(location.data[0]);
+    const latitude=location.data[0].lat;
+    const longitude=location.data[0].lon;
+    console.log(latitude);
+    console.log(longitude);
+    this.setState({display: true,
+                   srclink: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${latitude},${longitude}&zoom=13`})
+    console.log(this.state.srclink);
+    console.log(this.state.display);
+
+
   }
 
   render () {
@@ -28,10 +38,15 @@ class App extends React.Component {
       <header className="App-header">
         <form onSubmit={this.locationSearch}>
           <input onChange={(e) => this.setState({city: e.target.value})} 
-          name='lat' type='text' />
+          name='lat' type='text' placeholder='city' />
           <button>Explore!</button>
         </form>
-        <p>{this.state.city}</p>
+        <h2>Welcome</h2>
+         {this.state.display &&
+         <>
+         <h2>{this.state.city}</h2>
+         <img src={this.state.srclink} alt='map'/>
+         </>}
       </header>
     </div>
   )
