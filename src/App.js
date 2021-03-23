f2be5326e8f330633c44fafedb65cb6334b10877
jakoblumpkin <lumpkinjakobr@gmail.com>
2021-03-22 20:14:40 -0500

    ll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      location: {},
+      city: ''
+    }
+  }
+
+
+  locationSearch = async(e) => {
+    e.preventDefault();
+    this.setState({ city: e.target.lat.value });
+    const url=`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=&format=json`;
+    console.log(url);
+    const location = await axios.get(url);
+    console.log(location.data[0]);
+  }
+
+  render () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={this.locationSearch}>
+          <input onChange={(e) => this.setState({city: e.target.value})} 
+          name='lat' type='text' />
+          <button>Explore!</button>
+        </form>
+        <p>{this.state.city}</p>
       </header>
     </div>
-  );
+  )
+ }
 }
-
 export default App;
